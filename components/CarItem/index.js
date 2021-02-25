02e6bba2985521aae1d5358d413d5edd5c7f0f1b
yashguptaab99 <yashguptaab99@gmail.com>
2021-02-25 23:09:20 +0530

    Header and Flatlist done

--- components/CarItem/index.js
+++ components/CarItem/index.js
@@ -3,30 +3,43 @@ import { View, Text, ImageBackground} from 'react-native';
 import styles from './style'
 import StyledButton from '../StyledButton'
 const CarItem = (props) => {
+
+
+    const {name, tagline, taglineCTA, image} = props.car;
+
     return(
         <View style={styles.carContainer}>
             <ImageBackground
-                source={require('../../assets/images/ModelS.jpeg')}
+                source={image}
                 style={styles.image}
             />
 
             <View style={styles.titles}>
-                <Text style={styles.title}>Model S</Text>
-                <Text style={styles.subTitles}>Starting at $69,940</Text>
+                <Text style={styles.title}>{name}</Text>
+                <Text style={styles.subTitles}>
+                    {tagline}
+                    {' '}
+                    <Text style={styles.subTitlesCTA}>
+                        {taglineCTA}
+                    </Text>
+                </Text>
             </View>
 
-            <StyledButton 
-                type='primary' 
-                content={'Custom Order'}
-                onPress={() => {
-                    console.log("Custom Order was pressed!")
-            }}/>
-            <StyledButton 
-                type='secondary' 
-                content={'Existing Inventory'}
-                onPress={() => {
-                    console.log("Existing Inventory was pressed!")
-                }}/>
+            <View style={styles.buttonContainer}>
+                <StyledButton
+                    type='primary'
+                    content={'Custom Order'}
+                    onPress={() => {
+                        console.log("Custom Order was pressed!")
+                    }} />
+                <StyledButton
+                    type='secondary'
+                    content={'Existing Inventory'}
+                    onPress={() => {
+                        console.log("Existing Inventory was pressed!")
+                    }} />
+            </View>
+            
 
 
         </View>
