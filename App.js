5f117958a60acf2f8f938401acaac5cd22fb04cf
yashguptaab99 <yashguptaab99@gmail.com>
2021-02-25 10:17:07 +0530

    Render the background image

--- App.js
+++ App.js
@@ -1,11 +1,16 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, ImageBackground } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
       <View style={styles.carContainer}>
+        <ImageBackground 
+          source={require('./assets/images/ModelX.jpeg')}
+          style={styles.image}
+        />
+
         <View style={styles.titles}>
           <Text style={styles.title}>Model S</Text>
           <Text style={styles.subTitles}>Starting at $69,940</Text>
@@ -40,4 +45,10 @@ const styles = StyleSheet.create({
     fontSize: 16,
     color: '#5c5e62'
   },
+  image: {
+    width: '100%',
+    height: '100%',
+    resizeMode: 'cover',
+    position: 'absolute'
+  }
 });
