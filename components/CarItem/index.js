a655d806a5746646702487344dce86db961d8c0e
yashguptaab99 <yashguptaab99@gmail.com>
2021-02-25 11:57:30 +0530

    Button component is done

--- components/CarItem/index.js
+++ components/CarItem/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { View, Text, ImageBackground} from 'react-native';
 import styles from './style'
-
+import StyledButton from '../StyledButton'
 const CarItem = (props) => {
     return(
         <View style={styles.carContainer}>
@@ -14,6 +14,20 @@ const CarItem = (props) => {
                 <Text style={styles.title}>Model S</Text>
                 <Text style={styles.subTitles}>Starting at $69,940</Text>
             </View>
+
+            <StyledButton 
+                type='primary' 
+                content={'Custom Order'}
+                onPress={() => {
+                    console.log("Custom Order was pressed!")
+            }}/>
+            <StyledButton 
+                type='secondary' 
+                content={'Existing Inventory'}
+                onPress={() => {
+                    console.log("Existing Inventory was pressed!")
+                }}/>
+
 
         </View>
     )
---
+++ components/StyledButton/style.js
@@ -0,0 +1,22 @@
+import { StyleSheet } from 'react-native';
+
+const styles = StyleSheet.create({
+    container: {
+        width: '100%',
+        padding: 10,
+    },
+    text: {
+        fontSize: 12,
+        fontWeight: '500',
+        textTransform: 'uppercase'
+        
+    },
+    button: {
+        height: 40,
+        borderRadius: 20,
+        justifyContent: 'center',
+        alignItems:'center',
+    },
+});
+
+export default styles;
